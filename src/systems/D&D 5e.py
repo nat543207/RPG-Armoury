e1e9410c3db7153420e14fa60f221fa71b87a438
Nathan Joss (nat543207) <nate@jossfamily.com>
2016-04-26 04:17:29 -0500

    Preliminary designs/conceptual notes

---
+++ src/systems/D&D 5e.py
@@ -0,0 +1,23 @@
+class Character:
+    
+    def __init__(self):
+        pass
+
+class Race:
+    pass
+
+class Class:
+    pass
+
+class Item:
+    pass
+
+class Power:
+    pass
+
+class Feat:
+    pass
+
+class Skill:
+    pass
+  
